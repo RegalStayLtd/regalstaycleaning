6b23cd4d6c96634cbf37fd6e99a752439802855a
RegalStayLtd <mo@regalstay.co.uk>
2023-08-16 17:03:30 +0100

    Services

---
+++ assets/js/carousel.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    arrows: true,
+    dots: false,
+    pauseOnHover: false,
+    prevArrow: '<i class="slick-prev fa-solid fa-angle-left"></i>',
+    nextArrow: '<i class="slick-next fa-solid fa-angle-right"></i>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+});
